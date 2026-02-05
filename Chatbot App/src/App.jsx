23778ac36c0668bc6b034e081f2d6c5ac0b5b6a3
Abdellah <abdellah.elkoulali@gmx.fr>
2026-02-05 15:14:40 +0100

    Add components

--- Chatbot App/src/App.jsx
+++ Chatbot App/src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+function App() {  
+        const [chatMessages, setChatMessages] = useState([{
+          message: 'hello Chatbot',
+          sender: 'user',
+          id: 'id1'
+        },{
+          message: 'Hello! How can i help you?',
+          sender: 'robot',
+          id: 'id2'
+        },{
+          message: 'can you get me todays date?',
+          sender: 'user',
+          id: 'id3'
+        },{
+          message: 'Today is September 27',
+          sender: 'robot',
+          id: 'id4'
+      }]); 
+        // const [chatMessages, setChatMessages] = array; // array destructing
+       // const chatMessages = array[0];// current data
+       // const setChatMessages = array[1];//destructuring, update the data
+      
+        return (
+          <div className="app-container">          
+           <ChatMessages 
+            chatMessages={chatMessages}
+           />  
+            <ChatInput
+            chatMessages={chatMessages}
+            setChatMessages={setChatMessages}
+           />               
+          </div>
+        );
+      }
 
 export default App
